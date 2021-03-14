c49b902fcbd1da392dafea6bf88ae170e14e95f0
kroniak <me@kroniak.net>
2021-03-14 19:47:45 +0300

    update to net5

--- src/SampleClass.cs
+++ src/SampleClass.cs
@@ -3,88 +3,110 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace library {
-    public static class SampleClass {
+// ReSharper disable ParameterOnlyUsedForPreconditionCheck.Local
+// ReSharper disable CollectionNeverUpdated.Local
+// ReSharper disable ConvertIfStatementToReturnStatement
+// ReSharper disable once InconsistentNaming
+
+namespace library
+{
+    public static class SampleClass
+    {
         private const int MAX_URL_LENGTH = 1;
 
-        public static bool Method (bool a, bool b) {
+        public static bool Method(bool a, bool b)
+        {
             if (a && b)
                 return true;
-            else
-                return false;
+
+            return false;
         }
 
-        public static string Encode (string s) {
-            if (string.IsNullOrWhiteSpace (s)) return s;
+        public static string Encode(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return s;
 
-            if (s.Length > MAX_URL_LENGTH) {
+            if (s.Length > MAX_URL_LENGTH)
                 // ... other actions with s
 
                 return s;
-            }
+
             return s;
         }
 
-        private static bool ExceptionMethod (bool con) {
+        private static bool ExceptionMethod(bool con)
+        {
             if (!con)
-                throw new ArgumentException ("USER_DB is not path pattern user_adminXXXX",
-                    nameof (con));
+                throw new ArgumentException("USER_DB is not path pattern user_adminXXXX",
+                    nameof(con));
 
             return true;
         }
 
-        public static string LibraryMethod (bool con) {
-            try {
-                if (ExceptionMethod (con))
+        public static string LibraryMethod(bool con)
+        {
+            try
+            {
+                if (ExceptionMethod(con))
                     return "Successfully";
-            } catch (ArgumentException e) {
-                throw new Exception ("Something happened", e);
+            }
+            catch (ArgumentException e)
+            {
+                throw new Exception("Something happened", e);
             }
 
             return string.Empty;
         }
 
-        public static int ForMethod () {
+        public static int ForMethod()
+        {
             var index = 0;
-            while (true) {
+            while (true)
+            {
                 index++;
                 if (index == 10) // mutate to >=10  !=10    <10
-                {
                     break;
-                }
             }
 
             return index;
         }
 
-        public static int Limit (int limit) {
+        public static int Limit(int limit)
+        {
             if (limit < 0)
-                throw new ArgumentException ("Limit must be greater than 0", nameof (limit));
+                throw new ArgumentException("Limit must be greater than 0", nameof(limit));
 
             if (limit > 99) limit = 100;
 
             return limit;
         }
 
-        public static void Some<T> (object arg, IEnumerable<T> list) {
+        public static void Some<T>(object arg, IEnumerable<T> list)
+        {
             // ...
-            var result = new List<T> ();
+            var result = new List<T>();
             // ...
 
-            if (result.FirstOrDefault () != null) {
+            if (result.FirstOrDefault() != null)
+            {
                 // ...
             }
+
             // ...
         }
 
-        public static async Task<string> GetValueAsync (string url) {
-            var values = await GetFromUrlAsync (url);
+        public static async Task<string?> GetValueAsync(string url)
+        {
+            var values = await GetFromUrlAsync(url);
 
             // ... other work
 
-            return values.FirstOrDefault ();
+            return values.FirstOrDefault();
         }
 
-        private static Task<string[]> GetFromUrlAsync (string url) => Task.FromResult (new string[] { url });
+        private static Task<string[]> GetFromUrlAsync(string url)
+        {
+            return Task.FromResult(new[] {url});
+        }
     }
 }
--- test/SampleTest.cs
+++ test/SampleTest.cs
@@ -3,79 +3,88 @@ using System.Threading.Tasks;
 using library;
 using Xunit;
 
-namespace test {
-    public class SampleTest {
-
+namespace test
+{
+    public class SampleTest
+    {
         [Theory]
-        [InlineData (false, false)]
+        [InlineData(false, false)]
         // [InlineData (true, false)]
         // [InlineData (false, true)]
-        public void Method_Data_ReturnFalse (bool a, bool b) {
+        public void Method_Data_ReturnFalse(bool a, bool b)
+        {
             // Act
-            var result = SampleClass.Method (a, b);
+            var result = SampleClass.Method(a, b);
 
             // Assert
-            Assert.False (result);
+            Assert.False(result);
         }
 
         [Theory]
-        [InlineData (false)]
-        public void LibraryMethod_NegativeData_ThrowException (bool val) {
+        [InlineData(false)]
+        public void LibraryMethod_NegativeData_ThrowException(bool val)
+        {
             // Act
-            var ex = Assert.Throws<Exception> (() => SampleClass.LibraryMethod (val));
+            var ex = Assert.Throws<Exception>(() => SampleClass.LibraryMethod(val));
 
             // Assert
-            Assert.Equal ("Something happened", ex.Message);
-            Assert.Equal ("USER_DB is not path pattern user_adminXXXX (Parameter 'con')",
+            Assert.Equal("Something happened", ex.Message);
+            Assert.Equal("USER_DB is not path pattern user_adminXXXX (Parameter 'con')",
                 ex.InnerException?.Message);
         }
 
         [Theory]
-        [InlineData (true)]
-        public void LibraryMethod_PositiveData_ReturnCorrectString (bool val) {
+        [InlineData(true)]
+        public void LibraryMethod_PositiveData_ReturnCorrectString(bool val)
+        {
             // Act
-            var result = SampleClass.LibraryMethod (val);
+            var result = SampleClass.LibraryMethod(val);
 
             // Assert
-            Assert.Equal ("Successfully", result);
+            Assert.Equal("Successfully", result);
         }
 
         [Fact]
-        public void ForMethod_CorrectCheck () {
+        public void ForMethod_CorrectCheck()
+        {
             // Act
-            var i = SampleClass.ForMethod ();
+            var i = SampleClass.ForMethod();
 
             // Assert
-            Assert.Equal (10, i);
+            Assert.Equal(10, i);
         }
 
         [Theory]
-        [InlineData (100)]
-        [InlineData (101)]
-        public void Limit_Greater100_ReturnCorrectData (int value) {
-            Assert.Equal (100, SampleClass.Limit (value));
+        [InlineData(100)]
+        [InlineData(101)]
+        public void Limit_Greater100_ReturnCorrectData(int value)
+        {
+            Assert.Equal(100, SampleClass.Limit(value));
         }
 
         [Theory]
-        [InlineData (0)]
-        [InlineData (98)]
-        [InlineData (99)]
-        public void Limit_Smaller100_ReturnCorrectData (int value) {
-            Assert.Equal (value, SampleClass.Limit (value));
+        [InlineData(0)]
+        [InlineData(98)]
+        [InlineData(99)]
+        public void Limit_Smaller100_ReturnCorrectData(int value)
+        {
+            Assert.Equal(value, SampleClass.Limit(value));
         }
 
         [Theory]
-        [InlineData (-1)]
-        public void Limit_Smaller0_ThrowException (int value) {
-            Assert.Throws<ArgumentException> (() => SampleClass.Limit (value));
+        [InlineData(-1)]
+        public void Limit_Smaller0_ThrowException(int value)
+        {
+            Assert.Throws<ArgumentException>(() => SampleClass.Limit(value));
         }
 
         [Fact]
-        public async Task GetValue_ReturnCorrectDataAsync () {
+        public async Task GetValue_ReturnCorrectDataAsync()
+        {
             //... mock data
-            var result = await SampleClass.GetValueAsync ("some");
+            var result = await SampleClass.GetValueAsync("some");
 
-            Assert.Equal ("some", result);
+            Assert.Equal("some", result);
             //... some other assertion
         }
     }
